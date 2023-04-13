5491af006464a7b456188209b421af797f00ae1f
builde7b0b <ecbjr.it@gmail.com>
2023-04-13 12:07:19 -0700

    add player's name as param and var

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class HumanPlayer extends Player {
         private Scanner scanner;
         private String name;
+
+        public HumanPlayer(String name) {
+                scanner = new Scanner(System.in);
+                this.name = name;
+                
+                // set to keyboard by default
+                // create a new scanner to read from the keyboard essentially
+        }
         public int choose() {
                 //prompt user for choice
                 System.out.println("Enter 1 for rock, 2 for paper, and 3 for scissors");
